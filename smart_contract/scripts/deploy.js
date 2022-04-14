d057cd0a4aa4a36897d81bd488fbd965bc4e7f77
andrewhsiao11 <90870850+andrewhsiao11@users.noreply.github.com>
2022-04-14 09:28:34 -0400

    update deploy.js and setup .env

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -1,7 +1,15 @@
 const hre = require("hardhat");
 const main = async () => {
   const MultiSigWallet = await hre.ethers.getContractFactory("MultiSigWallet");
-  const multiSigWallet = await MultiSigWallet.deploy();
+  // initializing with 3 metamask accounts needing 2/3 for approval
+  const multiSigWallet = await MultiSigWallet.deploy(
+    [
+      "0x093Ed1B4028a4AcDD3E7FEFAA8c658a8601812dc",
+      "0x2d05BF7e0f0BE03bA852B9A758FF1D9c2Dd7E26a",
+      "0x84E399dbaF0b7D6dF49950C3a63879697E215a36",
+    ],
+    2
+  );
 
   await multiSigWallet.deployed();
   console.log("MultiSigWallet deployed to: ", multiSigWallet.address);
