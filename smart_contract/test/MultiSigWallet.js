9b54e0b358c070db3d14b80ac7485163e0ff9341
andrewhsiao11 <90870850+andrewhsiao11@users.noreply.github.com>
2022-04-14 21:14:19 -0400

    unit tests: complete revoke approval function

--- smart_contract/test/MultiSigWallet.js
+++ smart_contract/test/MultiSigWallet.js
@@ -119,6 +119,19 @@ describe("MultiSigWallet Contract", function () {
       expect(obj1.isExecuted).to.equal(false);
     });
 
+    it("Should require sender to have previously approved", async () => {
+      await submitTx();
+      await walletInstance.approveTransaction(0);
+      expect(await walletInstance.isApproved(0, acc1.address)).to.equal(true);
+    });
+
+     it("Should remove senders approval status", async () => {
+       await submitTx();
+       await walletInstance.approveTransaction(0);
+       await walletInstance.revokeApproval(0);
+       expect(await walletInstance.isApproved(0, acc1.address)).to.equal(false);
+     });
+
     it("Should decrement approval count by 1", async () => {
       await submitTx();
       await walletInstance.approveTransaction(0);
@@ -126,7 +139,6 @@ describe("MultiSigWallet Contract", function () {
       await walletInstance.revokeApproval(0);
       const obj2 = await walletInstance.transactions(0);
       expect(obj2.numApprovals).to.equal(obj1.numApprovals - 1);
-      expect(await walletInstance.isApproved(0, acc1.address)).to.equal(false);
     });
   });
 
@@ -155,7 +167,9 @@ describe("MultiSigWallet Contract", function () {
       const obj = await walletInstance.transactions(0);
 
       const approvalCount = parseInt(obj.numApprovals);
-      const approvalsRequired = parseInt(await walletInstance.numApprovalsRequired());
+      const approvalsRequired = parseInt(
+        await walletInstance.numApprovalsRequired()
+      );
 
       expect(approvalCount).to.be.greaterThanOrEqual(approvalsRequired);
     });
@@ -182,27 +196,26 @@ describe("MultiSigWallet Contract", function () {
       await submitTx();
       await walletInstance.approveTransaction(0);
       await walletInstance.connect(acc3).approveTransaction(0);
-       await acc1.sendTransaction({
-         to: walletInstance.address,
-         value: ethers.utils.parseEther("2.0"),
-       });
+      await acc1.sendTransaction({
+        to: walletInstance.address,
+        value: ethers.utils.parseEther("2.0"),
+      });
 
-       const acc2Balance1 = await parseInt(
-         ethers.utils.formatEther(
-           await ethers.provider.getBalance(acc2.address)
-         ));
-         
+      const acc2Balance1 = await parseInt(
+        ethers.utils.formatEther(await ethers.provider.getBalance(acc2.address))
+      );
+
       await walletInstance.executeTransaction(0);
 
       const acc2Balance2 = await parseInt(
         ethers.utils.formatEther(await ethers.provider.getBalance(acc2.address))
       );
-      
+
       const obj = await walletInstance.transactions(0);
       // transaction's value to be set to zero
       expect(parseInt(ethers.utils.formatEther(obj.amount))).to.equal(0.0);
       // acc2 balance to increase by 1 eth
-      expect(acc2Balance2).to.equal(acc2Balance1+1);
+      expect(acc2Balance2).to.equal(acc2Balance1 + 1);
     });
 
     it("Should decrement contract balance by amount sent", async () => {
@@ -257,9 +270,5 @@ describe("MultiSigWallet Contract", function () {
       // transaction's value to be set to zero
       expect(obj.isExecuted).to.equal(true);
     });
-
   });
-
-
-
 });
