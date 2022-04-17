a54333c2e2b29d7f98f52d3a0b7f3d79c83c5e1b
andrewhsiao11 <90870850+andrewhsiao11@users.noreply.github.com>
2022-04-17 11:35:21 -0400

    compile and deploy to Alchemy and set up connection files to frontend

---
+++ client/src/utils/constants.js
@@ -0,0 +1,7 @@
+import abi from "./MultiSigWallet.json";
+
+export const contractABI = abi.abi;
+export const contractAddress = "0xbfda2bB458160Fd07Ff241ee9c5220BD228a01ad";
+
+// got contract address from deployment and exporting it here
+// got ABI of contract from artifacts folder and copied it in utils and export it here
