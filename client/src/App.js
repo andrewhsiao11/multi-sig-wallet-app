abe59164f8513ed23248a94afb533f58382fa991
andrewhsiao11 <90870850+andrewhsiao11@users.noreply.github.com>
2022-04-17 15:05:05 -0400

    building out use context

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {MultiSigWalletContext} from './context/MultiSigWalletContext'
+import React, {useContext} from 'react'
+
 function App() {
+
+const {connectWallet, currentAccount} = useContext(MultiSigWalletContext)
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        
+        {!currentAccount && (
+        <button 
+        type='button'
+        onClick={connectWallet}
+        > Connect Wallet</button>) }
+
+
       </header>
     </div>
   );
---
+++ client/src/context/MultiSigWalletContext.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { contractABI, contractAddress } from "../utils/constants";
+
+//create react context
+export const MultiSigWalletContext = React.createContext();
+
+// since using metamask, get access to ethereum object
+// - destructure here ==> window.ethereum
+const { ethereum } = window;
+
+// fetch ethereum contract
+const getEthereumContract = () => {
+  // provider is metamask
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  // fetch contract with these three things
+  const MultiSigWalletContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
+};
+
+// contract provider always gets children
+// wrapping entire react application with all the data
+// thats getting passed into contract context
+// will have access to the value object
+export const MultiSigWalletProvider = ({ children }) => {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletisConnected = async () => {
+    try {
+      if (!ethereum) return alert("🦊 Please install metamask");
+      // get metamask connected accounts
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      // Check if there is at least one account connected
+      if (accounts.length) {
+        setCurrentAccount(accounts[0]);
+
+        // getAllTransactions()
+      } else {
+        console.log("😢 No accounts found");
+      }
+    } catch (error) {
+      console.log(error);
+      throw new Error("😢 No ethereum object");
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) return alert("🦊 Please install metamask");
+      // connect metamaks accounts to Dapp
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+      throw new Error("😢 No ethereum object");
+    }
+  };
+
+  const submitTransaction = async () => {
+      try {
+          if (!ethereum) return alert("🦊 Please install metamask");
+
+          //Submit trnsaction functionality
+          
+      } catch (error) {
+          console.log(error);
+          throw new Error("😢 No ethereum object");
+      }
+  }
+
+  useEffect(() => {
+    checkIfWalletisConnected();
+  }, []);
+
+  return (
+    <MultiSigWalletContext.Provider value={{ connectWallet, currentAccount }}>
+      {children}
+    </MultiSigWalletContext.Provider>
+  );
+};
